8b1c3146170705244affb4266b332a569e38c2df
ikirpichny <ikirpichny@mail.ru>
2021-06-24 11:12:46 +0300

    New updates for HW2

--- src/test/java/hw/two/AbstrSettingClass.java
+++ src/test/java/hw/two/AbstrSettingClass.java
@@ -3,19 +3,17 @@ package hw.two;
 import driverutils.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 public class AbstrSettingClass {
     public WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setupDriverClass() {
         driver = new ChromeDriverManager().setupChromeDriver();
     }
 
-    @AfterMethod
+    @AfterClass
     public void afterClass() {
         driver.quit();
     }
